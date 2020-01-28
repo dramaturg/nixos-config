0167dec6c3646195fb825c7ac1a92d8e1eea7961
Sebastian Krohn <472846+dramaturg@users.noreply.github.com>
2020-01-28 01:02:56 +0100

    zwerg wireguard

--- modules/wireguard.nix
+++ modules/wireguard.nix
@@ -34,6 +34,10 @@ in
           publicKey = "fFvx441OJ99/vJ1v3tcFc5kxw3EzBwrRSrzCPvw8NS4=";
           allowedIPs = [ "172.18.55.2/32" ];
         }
+        { # zwerg
+          publicKey = "Hf5WmvLuhpVKRC16ZqOfx6r1dQ4qa29GGmklCnNkWUI=";
+          allowedIPs = [ "172.18.55.3/32" ];
+        }
         { # sandkasten
           publicKey = "n/PJilqd0LplHSNVLgSSyH6s4PVE5cMCmK8uvoUwcAY=";
           allowedIPs = [ "172.18.55.57/32" ];
@@ -45,6 +49,7 @@ in
   networking.extraHosts = ''
     172.18.55.1 felsenberg.wireg
     172.18.55.2 woodstock.wireg
+    172.18.55.3 zwerg.wireg
     172.18.55.57 sandkasten.wireg
   '';
 }
