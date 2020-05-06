e1f8937b19c944adf62163be1afd8bb2f4519e0f
Sebastian Krohn <472846+dramaturg@users.noreply.github.com>
2020-05-06 23:05:39 +0200

    python virtualenv update

--- environments/python-virtualenv.nix
+++ environments/python-virtualenv.nix
@@ -1,27 +1,20 @@
 let
   pkgs = import <nixpkgs> {};
-  #nanomsg-py = .... build expression for this python library;
 in pkgs.mkShell {
   buildInputs = with pkgs; [
-    (python3.withPackages(ps: with ps; [
+    (python38.withPackages(ps: with ps; [
       pip
-      pathlib2
-      pafy
-      spotipy
-      mutagen
-      beautifulsoup4
-      unicode-slugify
-      titlecase
-      logzero
-      pyyaml
-      appdirs
     ]))
     pypi2nix
-    #nanomsg-py
   ];
   shellHook = ''
-    alias pip3="PIP_PREFIX='$(pwd)/_build/pip_packages' \pip"
-    export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.7/site-packages:$PYTHONPATH"
+    export PIP_PREFIX="$(pwd)/_build/pip_packages"
+    export PYTHONPATH="$(pwd)/_build/pip_packages/lib/python3.8/site-packages:$PYTHONPATH"
+    export PATH="$(pwd)/_build/pip_packages/bin:$PATH"
     unset SOURCE_DATE_EPOCH
+
+    pip install setuptools
+    echo pip install -r requirements.txt
+    echo pip install --install-option="--prefix=$PIP_PREFIX" -e .
   '';
 }
