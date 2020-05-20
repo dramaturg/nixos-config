14c5cb310d7404aacc204c4d2935f1166f928ce7
Sebastian Krohn <472846+dramaturg@users.noreply.github.com>
2020-05-20 21:47:18 +0200

    Cloud vendors

---
+++ hardware/oracle-vm.nix
@@ -0,0 +1,23 @@
+
+{ pkgs, lib, config, ... }:
+
+{
+  imports = [
+    <nixpkgs/nixos/modules/profiles/headless.nix>
+    <nixpkgs/nixos/modules/profiles/qemu-guest.nix>
+    ../modules/server.nix
+  ];
+
+  environment.systemPackages = with pkgs; [
+  ];
+
+  system.autoUpgrade.enable = lib.mkForce false;
+  users.mutableUsers = false;
+
+  boot = {
+    cleanTmpDir = true;
+    loader.grub.device = "/dev/sda";
+  };
+
+  fileSystems."/" = { device = "/dev/vda1"; fsType = "ext4"; };
+}
