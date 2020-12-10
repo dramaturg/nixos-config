e5f7bcfd828cc585bbdc1de6be53c938a2164d10
Sebastian Krohn <472846+dramaturg@users.noreply.github.com>
2020-12-10 21:58:40 +0100

    update fennel

--- environments/lua.nix
+++ environments/lua.nix
@@ -4,8 +4,8 @@ with luaPackages;
 let
   fennel = pkgs.writeScriptBin "fennel"
     ( builtins.readFile ( pkgs.fetchurl {
-      url = "https://fennel-lang.org/downloads/fennel-0.6.0";
-      sha256 = "01f0xfp9g5z6jhdr006n15bnn0rlcfdbgiilamqgkhx28gnjrswf";
+      url = "https://fennel-lang.org/downloads/fennel-0.7.0";
+      sha256 = "1j521x3qqxvrfard26abb1hpvfh9y245ha0kn4hz0cpmyqsw92q6";
     }));
 
   libs = [
