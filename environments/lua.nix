bb4fa34e8cc0134ef6854f0b23777df12f7f4f6f
Sebastian Krohn <472846+dramaturg@users.noreply.github.com>
2021-01-22 09:32:09 +0100

    fennel

--- environments/lua.nix
+++ environments/lua.nix
@@ -4,8 +4,8 @@ with luaPackages;
 let
   fennel = pkgs.writeScriptBin "fennel"
     ( builtins.readFile ( pkgs.fetchurl {
-      url = "https://fennel-lang.org/downloads/fennel-0.7.0";
-      sha256 = "1j521x3qqxvrfard26abb1hpvfh9y245ha0kn4hz0cpmyqsw92q6";
+      url = "https://fennel-lang.org/downloads/fennel-0.8.0";
+      sha256 = "0bv9sdhg7ahx6s48pnwiqgyxbqdyjcnhjjnwpyxdxmbcn8gq15g7";
     }));
 
   libs = [
