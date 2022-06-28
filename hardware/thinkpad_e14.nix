7dd3b44f4b9a8a054c1d3d96c3ddd62bd698d0a7
Sebastian Krohn <472846+dramaturg@users.noreply.github.com>
2022-06-28 22:47:41 +0200

    thinkpad e14

---
+++ hardware/thinkpad_e14.nix
@@ -0,0 +1,20 @@
+{ pkgs, lib, config, ... }:
+
+let
+  hardwareTarball = fetchTarball https://github.com/NixOS/nixos-hardware/archive/master.tar.gz;
+in
+{
+  imports = [
+    ./intel-generic.nix
+    ./intel-graphics.nix
+    <nixpkgs/nixos/modules/installer/scan/not-detected.nix>
+    (hardwareTarball + "/lenovo/thinkpad/e14")
+  ];
+
+  #hardware.trackpoint = {
+  #  enable = true;
+  #  sensitivity = 220;
+  #  speed = 0;
+  #  emulateWheel = true;
+  #};
+}
